50c8ec90b7724f8da67357ad7b559d463eb54666
Mateus Caetano <mateus-ramoshd@hotmail.com>
2025-05-07 10:24:08 -0300

    add go to home on logo img

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,28 @@ import { globalStyles } from "../styles/globals";
 import Image from "next/image";
 import logo from "../assets/logo.svg";
 import { Container, Header } from "../styles/pages/app";
+import { useRouter } from "next/router";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const handleBackToHome = () => {
+    router.push("/");
+  };
+
   return (
     <Container>
       <Header>
-        <Image src={logo} alt="" width={130} height={52} />
+        <Image
+          src={logo}
+          alt=""
+          width={130}
+          height={52}
+          onClick={handleBackToHome}
+          style={{ cursor: "pointer" }}
+        />
       </Header>
       <Component {...pageProps} />
     </Container>
