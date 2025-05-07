852fc6de22386946f5ca164b1e33e284b3daf68e
Mateus Caetano <mateus-ramoshd@hotmail.com>
2025-05-07 10:24:55 -0300

    add product detail page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -4,27 +4,107 @@ import {
   ProductContainer,
   ProductDetails,
 } from "../../styles/pages/product";
+import type { GetStaticPaths, GetStaticProps } from "next";
+import { getProductById, getProducts } from "../../lib/stripe";
+import Image from "next/image";
+import { Loading } from "../../components/loading";
+import Head from "next/head";
 
-export default function Product() {
-  const { query } = useRouter();
+import axios, { type AxiosError } from "axios";
+import { api } from "../../lib/axios";
+import { useState } from "react";
+
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    price: string;
+    image: string;
+    description: string;
+    priceId: string;
+  };
+}
+
+export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+    useState(false);
+  const { isFallback } = useRouter();
+
+  if (isFallback) {
+    return <Loading />;
+  }
+
+  async function handleBuyButton() {
+    try {
+      setIsCreatingCheckoutSession(true);
+
+      const response = await axios.post("/api/checkout", {
+        priceId: product.priceId,
+      });
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href = checkoutUrl;
+    } catch (err) {
+      setIsCreatingCheckoutSession(false);
+
+      console.log((err as AxiosError).response?.data);
+    }
+  }
 
   return (
-    <ProductContainer>
-      <ImageContainer></ImageContainer>
+    <>
+      <Head>{String(product.name)} | Ignite Shop</Head>
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.image} width={520} height={480} alt="" />
+        </ImageContainer>
 
-      <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
 
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
-          aliquid rerum exercitationem facere a molestiae ut sed velit non
-          mollitia? Officiis hic velit assumenda aspernatur nihil, sint sed
-          laboriosam tempora?
-        </p>
+          <p>{product.description}</p>
 
-        <button>Comprar agora</button>
-      </ProductDetails>
-    </ProductContainer>
+          <button
+            disabled={isCreatingCheckoutSession}
+            onClick={handleBuyButton}
+          >
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const products = await getProducts();
+
+  const paths = products.map((product) => {
+    return {
+      params: {
+        id: product.id,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params.id;
+
+  const product = await getProductById(productId);
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24,
+  };
+};
